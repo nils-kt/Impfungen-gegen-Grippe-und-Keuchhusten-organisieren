3be18057ca09093da27d199793915516c868cc95
Yannick <yannick.radke@gmail.com>
2020-03-21 00:59:14 +0100

    create a spring boot project.

---
+++ impfung/src/main/java/com/wirvsvirus/impfung/ImpfungApplication.java
@@ -0,0 +1,13 @@
+package com.wirvsvirus.impfung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpfungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpfungApplication.class, args);
+	}
+
+}
---
+++ impfung/src/test/java/com/wirvsvirus/impfung/ImpfungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wirvsvirus.impfung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpfungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
