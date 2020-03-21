258850e92b3dcab88be9f0922f9b01f7c82a731e
Nils <34674720+nils-kt@users.noreply.github.com>
2020-03-21 18:52:48 +0100

    Quick fixes

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom';
 import AppointmentWizard from 'components/appointment/AppointmentWizard'
 
 function App() {
-  return ReactDOM.render(
-    <AppointmentWizard />, 
-    document.getElementById("root"));
+    return ReactDOM.render(
+        <AppointmentWizard/>,
+        document.getElementById("root"));
 }
 
 export default App;
--- src/frontend/src/components/appointment/AppointmentWizard.js
+++ src/frontend/src/components/appointment/AppointmentWizard.js
@@ -1,14 +1,13 @@
-
 import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
+import {Container, Row, Col} from 'reactstrap';
 import ReactWizard from 'react-bootstrap-wizard-customized';
 
 import WelcomeStep from './steps/WelcomeStep';
 import SecondStep from './steps/SecondStep';
 
 const steps = [
-  WelcomeStep,
-  SecondStep
+    WelcomeStep,
+    SecondStep
 ];
 
 const title = 'Termin beantragen';
@@ -19,32 +18,32 @@ const next = 'Weiter';
 const submit = 'Termin beantragen';
 
 export default class AppointmentWizard extends React.Component {
-  finishButtonClick(allStates) {
-    console.log(allStates);
-  }
+    finishButtonClick(allStates) {
+        console.log(allStates);
+    }
 
-  render() {
-    return (
-      <Container fluid style={{ marginTop: "15px" }}>
-        <Row>
-          <Col xs={12} md={6} className="mr-auto ml-auto">
-            <ReactWizard
-              steps={steps}
-              navSteps
-              title={title}
-              description={description}
-              headerTextCenter
-              headerTextColor="#fff"
-              validate
-              color="primary"
-              finishButtonClick={this.finishButtonClick}
-              previousButtonText={prev}
-              nextButtonText={next}
-              finishButtonText={submit}
-            />
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
+    render() {
+        return (
+            <Container fluid style={{marginTop: "15px"}}>
+                <Row>
+                    <Col xs={12} md={6} className="mr-auto ml-auto">
+                        <ReactWizard
+                            steps={steps}
+                            navSteps
+                            title={title}
+                            description={description}
+                            headerTextCenter
+                            headerTextColor="#fff"
+                            validate
+                            color="primary"
+                            finishButtonClick={this.finishButtonClick}
+                            previousButtonText={prev}
+                            nextButtonText={next}
+                            finishButtonText={submit}
+                        />
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
 }
