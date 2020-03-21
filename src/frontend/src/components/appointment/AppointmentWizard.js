8c2a1d49341697539566844d9afafcb86f92ceb3
Steven Agyekum <burnett@posteo.de>
2020-03-21 16:39:50 +0100

    Moved Wizard logic to separate component

---
+++ src/frontend/src/components/appointment/AppointmentWizard.js
@@ -0,0 +1,50 @@
+
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import ReactWizard from 'react-bootstrap-wizard';
+
+import WelcomeStep from './steps/WelcomeStep';
+import SecondStep from './steps/SecondStep';
+
+const steps = [
+  WelcomeStep,
+  SecondStep
+];
+
+const title = 'Termin beantragen';
+const description = 'xxx';
+
+const prev = 'Zurück';
+const next = 'Weiter';
+const submit = 'Termin beantragen';
+
+export default class AppointmentWizard extends React.Component {
+  finishButtonClick(allStates) {
+    console.log(allStates);
+  }
+
+  render() {
+    return (
+      <Container fluid style={{ marginTop: "15px" }}>
+        <Row>
+          <Col xs={12} md={6} className="mr-auto ml-auto">
+            <ReactWizard
+              steps={steps}
+              navSteps
+              title={title}
+              description={description}
+              headerTextCenter
+              validate
+              progressbar="false"
+              color="primary"
+              finishButtonClick={this.finishButtonClick}
+              previousButtonText={prev}
+              nextButtonText={next}
+              finishButtonText={submit}
+            />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
