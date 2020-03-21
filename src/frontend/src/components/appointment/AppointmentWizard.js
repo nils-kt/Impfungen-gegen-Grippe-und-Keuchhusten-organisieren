b5f09afe704b0bfb3bac528f556c554f88064fe4
Nils <34674720+nils-kt@users.noreply.github.com>
2020-03-21 19:51:48 +0100

    Missing button class

--- src/frontend/src/components/appointment/AppointmentWizard.js
+++ src/frontend/src/components/appointment/AppointmentWizard.js
@@ -6,44 +6,45 @@ import WelcomeStep from './steps/WelcomeStep';
 import SecondStep from './steps/SecondStep';
 
 const steps = [
-    WelcomeStep,
-    SecondStep
+  WelcomeStep,
+  SecondStep
 ];
 
 const title = 'Termin beantragen';
-const description = 'xxx';
+const description = '';
 
 const prev = 'Zurück';
 const next = 'Weiter';
 const submit = 'Termin beantragen';
 
 export default class AppointmentWizard extends React.Component {
-    finishButtonClick(allStates) {
-        console.log(allStates);
-    }
+  finishButtonClick(allStates) {
+    console.log(allStates);
+  }
 
-    render() {
-        return (
-            <Container fluid style={{marginTop: "15px"}}>
-                <Row>
-                    <Col xs={12} md={6} className="mr-auto ml-auto">
-                        <ReactWizard
-                            steps={steps}
-                            navSteps
-                            title={title}
-                            description={description}
-                            headerTextCenter
-                            headerTextColor="#fff"
-                            validate
-                            color="primary"
-                            finishButtonClick={this.finishButtonClick}
-                            previousButtonText={prev}
-                            nextButtonText={next}
-                            finishButtonText={submit}
-                        />
-                    </Col>
-                </Row>
-            </Container>
-        );
-    }
+  render() {
+    return (
+      <Container fluid style={{marginTop: "15px"}}>
+        <Row>
+          <Col xs={12} md={6} className="mr-auto ml-auto">
+            <ReactWizard
+              steps={steps}
+              navSteps
+              title={title}
+              description={description}
+              headerTextCenter
+              headerTextColor="#fff"
+              validate
+              color="primary"
+              finishButtonClick={this.finishButtonClick}
+              previousButtonText={prev}
+              nextButtonText={next}
+              finishButtonText={submit}
+              finishButtonClasses="btn-info"
+            />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
