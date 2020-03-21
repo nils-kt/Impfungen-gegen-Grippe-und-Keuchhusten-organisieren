05f85e415b531b160996fde8b087e6e925571019
Steven Agyekum <burnett@posteo.de>
2020-03-21 17:05:47 +0100

    Termin -> Appointment

--- src/src/main/java/org/wirvsvirushackathon/impfung/AppointmentRepository.java
+++ src/src/main/java/org/wirvsvirushackathon/impfung/AppointmentRepository.java
@@ -1,13 +1,13 @@
 package org.wirvsvirushackathon.impfung;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.wirvsvirushackathon.impfung.Entity.Termin;
+import org.wirvsvirushackathon.impfung.Entity.Appointment;
 
 /**
  * Hier findet der Zugriff auf die Datenbank statt.
  * @author Yannick
  *
  */
-public interface TerminRepository extends MongoRepository<Termin, String> {
+public interface AppointmentRepository extends MongoRepository<Appointment, String> {
 
 }
