23a7661b708f6d470b5cebf5e9c0ec6a66c19ca4
Steven Agyekum <burnett@posteo.de>
2020-03-21 14:23:34 +0100

    add window.fetch wrapper class

---
+++ src/frontend/src/classes/Api.js
@@ -0,0 +1,52 @@
+/**
+ * Tiny wrapper around window.fetch().
+ * @author Steven Agyekum <Burnett01>
+ * @class
+ */
+export class Api {
+  /**
+   * Creates an Api instance.
+   * @param {String} baseUrl 
+   */
+  constructor (baseUrl) {
+    this.baseUrl = baseUrl
+  }
+  /**
+   * Performs a fetch request.
+   * The return value (Array) should be
+   * destructured by the caller.
+   * @param {String} path - The path
+   * @param {Object} opts - The options
+   * @returns {Array} - [true/false, data/err]
+   */
+  request(path, opts) {
+    try {
+      const res = await fetch(baseUrl+path, opts)
+      if (!res.ok) throw new Error(res)
+      return [true, res]
+    } catch (err) {
+      return [false, err]
+    }
+  }
+  /**
+   * API wrapper for GET.
+   * @param {String} path - The path
+   * @param {Object} opts - The options
+   */
+  get(path, opts) {
+    return this.request(path, opts)
+  }
+  /**
+   * API wrapper for POST.
+   * @param {String} path - The path
+   * @param {String} body - The payload 
+   * @param {Object} opts - The options
+   */
+  post(path, body, opts) {
+    return this.request(path, { 
+      ...opts,
+      body,
+      method: 'POST'
+    })
+  }
+}
