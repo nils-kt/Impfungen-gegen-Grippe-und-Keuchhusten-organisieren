3795f3e3fc0edc758001089499d04df6f14e405a
Yannick <yannick.radke@gmail.com>
2020-03-22 02:11:53 +0100

    added RequiredArgsConstructor and deleted modelAndView.

--- src/src/main/java/org/wirvsvirushackathon/Controller/AppointmentController.java
+++ src/src/main/java/org/wirvsvirushackathon/Controller/AppointmentController.java
@@ -1,16 +1,15 @@
 package org.wirvsvirushackathon.Controller;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 import org.wirvsvirushackathon.impfung.Entity.Appointment;
 import org.wirvsvirushackathon.impfung.Repository.AppointmentRepository;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class AppointmentController {
@@ -23,16 +22,6 @@ public class AppointmentController {
 	private void addAppointment() {
 		Appointment appointmentEntry = new Appointment(LocalDateTime.now());
 		appointmentRepository.save(appointmentEntry);
-	}
-
-	@RequestMapping(method = RequestMethod.GET, value = "/appointments")
-	ModelAndView appointmentEntries() {
-
-		ModelAndView modelAndView = new ModelAndView("appointmentEntries");
-		modelAndView.addObject("title", "Appointment");
-		modelAndView.addObject("appointmentEntries", appointmentRepository.findAll());
-
-		return modelAndView;
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/get")
